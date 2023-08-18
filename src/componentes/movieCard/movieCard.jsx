ef2774f86a1e58cbf5eeab0216d03851482c7ef3
cainhocr <caioignacio086@gmail.com>
2023-08-18 14:58:19 -0300

    finalização do botão e do movieCard

--- src/componentes/movieCard/movieCard.jsx
+++ src/componentes/movieCard/movieCard.jsx
@@ -1,23 +1,29 @@
+import MovieDesc from "../moviedescription/movieDescription";
 import style from "./movieCard.module.css";
 
 const MovieCard = ({ movies }) => {
   const movie = movies;
   return (
-    <div className={style.movie}>
-      <div>
-        <p>{movie.Year}</p>
+    <>
+      <div className={style.movie}>
+        <div>
+          <p>{movie.Year}</p>
+        </div>
+        <div>
+          <img
+            src={
+              movie.Poster ? movie.Poster : "https://via.placeholder.com/400"
+            }
+            alt={movie.Title}
+          />
+        </div>
+        <div>
+          <span>{movie.Type}</span>
+          <h3>{movie.Title}</h3>
+        </div>
       </div>
-      <div>
-        <img
-          src={movie.Poster ? movie.Poster : "https://via.placeholder.com/400"}
-          alt={movie.Title}
-        />
-      </div>
-      <div>
-        <span>{movie.Type}</span>
-        <h3>{movie.Title}</h3>
-      </div>
-    </div>
+      <MovieDesc movies={movie}/>
+    </>
   );
 };
 export default MovieCard;
